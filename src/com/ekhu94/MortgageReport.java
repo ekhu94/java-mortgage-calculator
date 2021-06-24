4d3f10eda89b13f94d227fb860a6ce0a06f6d5dd
Erik Huang <ekhu94@gmail.com>
2021-06-24 09:04:26 -0700

    final refactor to currency field of NumberFormat

--- src/com/ekhu94/MortgageReport.java
+++ src/com/ekhu94/MortgageReport.java
@@ -5,22 +5,24 @@ import java.text.NumberFormat;
 public class MortgageReport {
 
     private MortgageCalculator calculator;
+    private final NumberFormat currency;
 
     public MortgageReport(MortgageCalculator calculator) {
         this.calculator = calculator;
+        this.currency = NumberFormat.getCurrencyInstance();
     }
     public void printMortgage() {
         double mortgage = calculator.calculateMortgage();
         System.out.println("MORTGAGE");
         System.out.println("--------");
-        System.out.println("Monthly Payments: " + NumberFormat.getCurrencyInstance().format(mortgage));
+        System.out.println("Monthly Payments: " + currency.format(mortgage));
     }
 
     public void printPaymentSchedule() {
         System.out.println("PAYMENT SCHEDULE");
         System.out.println("----------------");
         for (double balance : calculator.getRemainingBalances()) {
-            System.out.println(NumberFormat.getCurrencyInstance().format(balance).trim());
+            System.out.println(currency.format(balance));
         }
     }
 }
